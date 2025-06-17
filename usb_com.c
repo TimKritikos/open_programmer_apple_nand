99a43bb1ae62e2d85d78d3c97894ac9931484ce9
(Tim) Efthimis Kritikos <git-27@mail.efthimiskritikos.com>
2025-06-17 03:01:09 +0100

    Added copyright and license notices to the source code files

--- usb_com.c
+++ usb_com.c
@@ -1,3 +1,22 @@
+/* usb_com.c - Creating an abstraction layer of the communication and the programmer
+
+   This file is part of the Open Programmer for Apple NANDs project
+
+   Copyright (c) 2025 Efthymios Kritikos
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
 #include <libusb-1.0/libusb.h>
 #include <string.h>
 #include <stdio.h>
